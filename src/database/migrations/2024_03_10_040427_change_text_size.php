70df0ac7cfa3ec2b6459d14f03b475ec0a74e07e
batsoyombo-dev <batsoyombo.kh@gmail.com>
2024-03-10 12:05:28 +0800

    change

---
+++ src/database/migrations/2024_03_10_040427_change_text_size.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table("v3_courses", function (Blueprint $table) {
+            $table->string("description", 1024)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table("v3_courses", function (Blueprint $table) {
+            $table->string("description", 255)->change();
+        });
+    }
+};
