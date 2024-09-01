c37392ba1df4b6485c199b5a2057cd64a6ab6e04
batsoyombo-dev <batsoyombo.kh@gmail.com>
2024-09-01 11:37:49 +0800

    change

---
+++ src/database/migrations/2024_09_01_113529_add_soft_delete_block.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table("v3_course_group_blocks", function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table("v3_course_groups", function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table("v3_courses", function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table("v3_course_group_blocks", function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+
+        Schema::table("v3_course_groups", function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+
+        Schema::table("v3_courses", function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+    }
+};
